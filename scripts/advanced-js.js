58aa8e1883cbe56a4e719bb7d29fe04460b74ee2
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-05-21 15:13:42 +0100

    add data processing task

--- scripts/advanced-js.js
+++ scripts/advanced-js.js
@@ -3,25 +3,57 @@
  */
 //1. Basic setup & Message Passing
 
-if (window.Worker) {
-  const myWorker = new Worker("worker.js"); //Instantiate new worker
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.Worker) {
+    const myWorker = new Worker("worker.js"); //Instantiate new worker
 
-  myWorker.onmessage = function (e) {
+    myWorker.onmessage = function (event) {
+      document.getElementById(
+        "myWorkerReceived"
+      ).textContent = `New message received from worker: ${event.data}`;
+    };
+
+    const arr = [5, 10];
+    myWorker.postMessage({ task: "createAndMessage", data: arr });
     document.getElementById(
-      "myWorkerReceived"
-    ).textContent = `New message received from worker: ${e.data}`;
-  };
-
-  arr = [5, 10];
-  myWorker.postMessage(arr);
-  document.getElementById(
-    "myWorkerSent"
-  ).textContent = `New message sent to worker: [${arr}]`;
-} else {
-  document.getElementById(
-    "errorMessage"
-  ).textContent = `Window does not support worker`;
-}
+      "myWorkerSent"
+    ).textContent = `New message sent to worker: [${arr}]`;
+  } else {
+    document.getElementById(
+      "errorMessage"
+    ).textContent = `Window does not support worker`;
+  }
+});
 
 //2. Data Processing Task
 //Note: To access a csv file in your directory: 1. initialize node.js (npm init -y); 2. install csv-parser (npm install csv-parser)
+document.getElementById("csvFile").addEventListener("change", function (e) {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = function (event) {
+    const csvData = event.target.result;
+
+    processCSV(csvData);
+  };
+  reader.readAsText(file);
+});
+
+function processCSV(csvData) {
+  if (window.Worker) {
+    const myCSVWorker = new Worker("worker.js");
+
+    myCSVWorker.postMessage({ task: "processCSV", data: csvData }); //send file to worker
+    document.getElementById(
+      "sendCSV"
+    ).textContent = `CSV file sent to the CSV Web Worker successfully!`; //alert that message sent
+
+    myCSVWorker.onmessage = function (event) {
+      //receive result from worker
+      const csvResult = event.data;
+      document.getElementById(
+        "csvResult"
+      ).textContent = `Total Sales: ${csvResult} received from CSV Web Worker Successfully!`;
+    };
+  }
+}
--- templates/worker.js
+++ templates/worker.js
@@ -1,8 +1,74 @@
 //New web worker
-self.onmessage = function (e) {
-  console.log(`New message received from main thread ${e.data}`); //worker confirms task received
+self.onmessage = function (event) {
+  const { task, data } = event.data;
 
-  const result = e.data[0] * e.data[1]; //worker computes the task
-
-  self.postMessage(result); //worker sends result to main
+  switch (task) {
+    case "createAndMessage":
+      createAndMessage(data);
+      break;
+    case "processCSV":
+      processCSV(data);
+      break;
+    default:
+      self.postMessage({ error: "Unknown Task" });
+  }
 };
+
+function createAndMessage(data) {
+  console.log(`New message received from main thread ${data}`); //worker confirms task received
+  if (
+    Array.isArray(data) &&
+    (data.length === 2) & !isNaN(data[0]) &&
+    !isNaN(data[1])
+  ) {
+    const result = data[0] * data[1]; //worker computes the task
+    self.postMessage(result); //worker sends result to main
+  } else {
+    self.postMessage({ error: "Invalid data" }); //worker sends result to main
+  }
+}
+
+function processCSV(data) {
+  console.log(`processCSV function initialized successfully`);
+  const rows = data.split("\n");
+  const headers = rows[0].split(",");
+  headers = headers.map((header) => header.toLowerCase());
+
+  const quantityIndex = lowercase.headers.indexOf("quantity");
+  const unitPriceIndex = lowercase.headers.indexOf("unit price");
+  if (quantityIndex < 0 || unitPriceIndex < 0) {
+    console.log(`Second if statement passed successfully`);
+    console.log(
+      `quantityIndex: ${quantityIndex}, unitPriceIndex: ${unitPriceIndex}`
+    );
+    self.postMessage({
+      error:
+        'Columns not found: please ensure csv contains "quantity" & "unit price" column in that format.',
+    });
+    return;
+  }
+
+  const csvResult = [];
+  console.log(`csvResult Array initialized successfully`);
+  for (let row of rows.slice(1)) {
+    // Start from index 1 to skip header row
+    const rowItems = row.split(",");
+
+    if (rowItems.length > Math.max(quantityIndex, unitPriceIndex)) {
+      const quantity = parseFloat(rowItems[quantityIndex]);
+      const unitPrice = parseFloat(rowItems[unitPriceIndex]);
+      csvResult.push({ quantity, unitPrice });
+    }
+  }
+
+  const totalSales = csvResult.reduce(
+    (sum, item) => sum + item.quantity * item.unitPrice,
+    0
+  );
+
+  console.log(`Total sales ${totalSales}`);
+  self.postMessage({
+    totalSales: totalSales, // Sending the total sales value as part of an object
+  });
+  console.log(`Total sales sent successfully`);
+}
