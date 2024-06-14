ea27a24abbcb68808715961aa1837b7b320c3936
IGWE, Chibueze C. <scientistigwe@gmail.com>
2024-06-14 23:17:04 +0100

    modify to exclude backend

--- backend/monitoring/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import SensorData
-
-class SensorDataSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SensorData
-        fields = '__all__'
